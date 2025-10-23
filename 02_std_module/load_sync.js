db0414d230ad92b107c7cc41d25752df9e3f8fe6
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-10-23 17:40:22 +0900

    update

--- 02_std_module/load_sync.js
+++ 02_std_module/load_sync.js
@@ -1,15 +1,17 @@
 /**
  * モジュール読み込み（CommonJS）
  */
-// TODO: fs モジュールを読み込む: CommonJS形式
+// TODO: fsモジュール読み込む: CommonJS形式
+const fs = require('fs');
 
-// TODO: path モジュールを読み込む: CommonJS形式
+// TODO: pathモジュールを読み込む: CommonJS形式
+const path = require('path');
 
 /**
  * ファイル読み込み処理
  */
 // TODO: 現在のディレクトリパスから、data/products.json を指定
-const filePath = "";
+const filePath = path.join(__dirname, 'data', 'products.json');
 
 // 1) 最初に実行
 console.log("📖 ファイル読み込み開始（同期）...");
@@ -17,7 +19,7 @@ console.log("📖 ファイル読み込み開始（同期）...");
 try {
     // 2) 同期でファイル読み込み
     // TODO: readFileSync(パス, 文字コード)
-    const data = {};
+    const data = fs.readFileSync(filePath, 'utf-8');
 
     // 3) 読み込み完了後に実行
     show(data);
