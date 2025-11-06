2b97f9a417e8694a0a00c88e406d8678b3e19fa4
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-06 18:30:11 +0900

    update

--- 04_npm/crypt.js
+++ 04_npm/crypt.js
@@ -1,13 +1,15 @@
-// TODO: カスタム暗号化モジュールのインポート: ./utils/cryptoUtil.js
+// TODO: カスタム暗号化モジュールのインポート: ./utils/cryptoUtil
 // メソッド: encrypt, decrypt, generateKey
+// req で入力補完
+const { encrypt, decrypt, generateKey } = require('./utils/cryptoUtil');
 
-// 鍵（32文字＝256bit）、初期化ベクトル（16文字＝128bit）
 const message = "Hello";
+// 鍵の生成
 const key = generateKey("mySecretKey");
 // TODO: メッセージを暗号化
-const encrypted = "";
+const encrypted = encrypt(message, key);
 // TODO: 暗号文を復号化
-const decrypted = "";
+const decrypted = decrypt(encrypted, key);
 
 // 結果表示
 let result = { message, encrypted, decrypted };
